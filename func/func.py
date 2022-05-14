4e74fcf46be55503b4eca2797e15777fd086c0d7
tan <tanz.cntt@gmail.com>
2022-05-15 04:14:14 +0700

    Initial commit

---
+++ func/func.py
@@ -0,0 +1,85 @@
+import pyautogui
+
+
+def check_screen():
+    try:
+        if pyautogui.locateOnScreen("./resources/screens/victory.png", confidence=0.9):
+            return {
+                "name": "victory",
+                "data": {}
+            }
+    except:
+        pass
+    try:
+        if pyautogui.locateOnScreen("./resources/screens/battling.png", confidence=0.9):
+            return {
+                "name": "battling",
+                "data": {}
+            }
+    except:
+        pass
+    try:
+        if pyautogui.locateOnScreen("./resources/screens/battle.png", confidence=0.9):
+            return {
+                "name": "battle",
+                "data": {}
+            }
+    except:
+        pass
+    try:
+        if pyautogui.locateOnScreen("./resources/screens/formation.png", confidence=0.9):
+            lvl = "easy"
+            return {
+                "name": "formation",
+                "data": {}
+            }
+    except:
+        pass
+    try:
+        if pyautogui.locateOnScreen("./resources/screens/home.png", confidence=0.9):
+            return {
+                "name": "home",
+                "data": {}
+            }
+    except:
+        pass
+    try:
+        if pyautogui.locateOnScreen("./resources/screens/story.png", confidence=0.9):
+            stage = 1
+            lvl = "easy"
+            if pyautogui.locateOnScreen("./resources/stages/1.png", confidence=0.9):
+                stage = 1
+            elif pyautogui.locateOnScreen("./resources/stages/2.png", confidence=0.9):
+                stage = 2
+            elif pyautogui.locateOnScreen("./resources/stages/3.png", confidence=0.9):
+                stage = 3
+            elif pyautogui.locateOnScreen("./resources/stages/4.png", confidence=0.9):
+                stage = 4
+            elif pyautogui.locateOnScreen("./resources/stages/5.png", confidence=0.9):
+                stage = 5
+            elif pyautogui.locateOnScreen("./resources/stages/6.png", confidence=0.9):
+                stage = 6
+
+            if pyautogui.locateOnScreen("./resources/stages/easy.png", confidence=0.9):
+                lvl = "easy"
+            elif pyautogui.locateOnScreen("./resources/stages/normal.png", confidence=0.9):
+                lvl = "normal"
+            elif pyautogui.locateOnScreen("./resources/stages/hard.png", confidence=0.9):
+                lvl = "hard"
+            elif pyautogui.locateOnScreen("./resources/stages/hell.png", confidence=0.9):
+                lvl = "hell"
+
+            return {
+                "name": "story",
+                "data": {
+                    "lvl": lvl,
+                    "stage": stage
+                }
+            }
+    except:
+        pass
+
+    return {
+        "name": "404",
+        "data": {}
+    }
---
+++ main.py
@@ -0,0 +1,62 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+import time
+
+import pyautogui
+
+from func.action import *
+from func.func import *
+
+
+def run_bot(name):
+    # Use a breakpoint in the code line below to debug your script.
+
+    while True:
+        screen = check_screen()
+        if screen['name'] == 'home':
+            status = start_farm(stage=6, lvl="normal")
+            if not status:
+                pos1 = pyautogui.locateCenterOnScreen("./resources/buttons/x-btn.png", confidence=0.9)
+                pyautogui.click(x=pos1.x, y=pos1.y)
+                time.sleep(1)
+                break
+        print(screen)
+        # pos1 = pyautogui.locateCenterOnScreen("./resources/buttons/back-button.png", confidence=0.9)
+        # pyautogui.click(x=pos1.x, y=pos1.y)
+        time.sleep(1)
+
+    while True:
+        screen = check_screen()
+        if screen['name'] == 'home':
+            status = start_farm(stage=3, lvl="normal")
+            if not status:
+                pos1 = pyautogui.locateCenterOnScreen("./resources/buttons/x-btn.png", confidence=0.9)
+                pyautogui.click(x=pos1.x, y=pos1.y)
+                time.sleep(1)
+                break
+        print(screen)
+        # pos1 = pyautogui.locateCenterOnScreen("./resources/buttons/back-button.png", confidence=0.9)
+        # pyautogui.click(x=pos1.x, y=pos1.y)
+        time.sleep(1)
+    while True:
+        screen = check_screen()
+        if screen['name'] == 'home':
+            status = start_farm(stage=3, lvl="easy")
+            if not status:
+                pos1 = pyautogui.locateCenterOnScreen("./resources/buttons/x-btn.png", confidence=0.9)
+                pyautogui.click(x=pos1.x, y=pos1.y)
+                time.sleep(1)
+                break
+        print(screen)
+        # pos1 = pyautogui.locateCenterOnScreen("./resources/buttons/back-button.png", confidence=0.9)
+        # pyautogui.click(x=pos1.x, y=pos1.y)
+        time.sleep(1)
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    run_bot('PyCharm')
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
