68b48b35ce34309b1d06d7ed33353ec429140745
tannn <tanz.cntt@gmail.com>
2022-06-05 01:02:32 +0700

    update

--- func/click.py
+++ func/click.py
@@ -7,7 +7,7 @@ def custom_click(x, y):
     if system_name == "Linux":
         pyautogui.click(x=x, y=y)
         return True
-    if system_name == "Window":
+    if system_name == "Windows":
         pyautogui.moveTo(x=x, y=y)
         pyautogui.sleep(1)
         pyautogui.click()
